a28d04c48235d2805a7be18579f6773d48fd2ed6
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-06-06 13:56:53 -0300

    feature: finishes transaction row component

--- src/app/components/transactions-history/TransactionRow.tsx
+++ src/app/components/transactions-history/TransactionRow.tsx
@@ -13,13 +13,36 @@ interface TransactionRowProps {
 }
 
 export default function TransactionRow({ transaction }: TransactionRowProps) {
+    let typeBackgroundColorClass = "";
+    let amountColorClass = "";
+    let amountPrefix = "";
+
+    switch(transaction.type) {
+        case "income":
+            typeBackgroundColorClass = "bg-black";
+            amountColorClass = "text-green-600";
+            amountPrefix = "+";
+            break;
+        default:
+            typeBackgroundColorClass = "bg-red-600";
+            amountColorClass = "text-red-600";
+            amountPrefix = "-";
+            break;
+    }
+
     return (
         <TableRow>
             <TableCell>{ transaction.date }</TableCell>
-            <TableCell>{ transaction.type }</TableCell>
+            <TableCell>
+                <span className={
+                    `${typeBackgroundColorClass} text-white rounded-3xl text-center py-1 px-4`
+                }>
+                    { transaction.type }
+                </span>
+            </TableCell>
             <TableCell>{ transaction.category }</TableCell>
             <TableCell>{ transaction.description }</TableCell>
-            <TableCell>{ transaction.amount }</TableCell>
+            <TableCell className={`${amountColorClass} font-medium`}>{amountPrefix}${ transaction.amount.toLocaleString() }</TableCell>
         </TableRow>
     );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import UserProvider from "@/contexts/UserContext";
 import AlertProvider from "@/contexts/AlertContext";
 import GlobalAlert from "@/components/GlobalAlert";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const inter = Inter({
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
@@ -29,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
+        className={`${inter.className} antialiased bg-gray-50`}
       >
         <AlertProvider>
           <UserProvider>
